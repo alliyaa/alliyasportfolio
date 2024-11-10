4a6256e640a168cec340ccbbbb23c30b6088aae6
alliyaa <alliyaahmad#@gmail.com>
2024-11-10 16:38:41 +0300

    created files and added initial hero design

---
+++ components/Hero.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Spotlight } from './ui/Spotlight'
+
+function Hero() {
+  return (
+    <div className="pb-20 pt-36">
+      <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
+      <Spotlight className="top-10 left-full h-80[vh] w-50[vw]" fill="purple" />
+      <Spotlight className="top-28 left-80 h-80[vh] w-50[vw] " fill="blue" />
+    </div>
+  )
+}
+
+export default Hero
